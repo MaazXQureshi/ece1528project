eef8a5fcc78ea502cb9ee86433c28c3a62404830
Charles Hsieh <1hsiehcha@gmail.com>
2025-10-30 19:41:25 -0400

    [Project] Added the code to send MPU6050 data over to MQTT in JSON format.

--- sensor_folder/mpu6050/mpu6050.ino
+++ sensor_folder/mpu6050/mpu6050.ino
@@ -1,8 +1,26 @@
+// For MQTT transmissions:
+#include "MqttClient.h"
+#include "WifiClient.h"
+#include <ArduinoJson.h>
+
+// For MPU-6050:
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 Adafruit_MPU6050 mpu;
+
+// Wi-Fi login credientials (should modify according to the your Wi-Fi).
+char ssid_wifi[] = "BELL318";
+char pass_wifi[] = "A2761E9A66C7";
+WifiClient wifi_client(ssid_wifi, pass_wifi);
+
+// MQTT publisher settings. Be sure to change the IP address!
+const char* mqtt_broker_ip = "192.168.2.35";
+const int mqtt_broker_port = 1883;
+const char* client_id = "publisher_MPU6050";
+const char* publish_topic = "MPU6050";
+MqttClient mqtt_client(mqtt_broker_ip, mqtt_broker_port);
 
 void setup(void) {
   Serial.begin(115200);
@@ -12,7 +30,9 @@ void setup(void) {
     delay(10);
   }
 
-  Serial.println("Adafruit MPU6050 test!");
+  // Attempt to connect to the Wi-Fi and MQTT.
+  wifi_client.connect();
+  mqtt_client.connect(client_id);
 
   // Try to initialize!
   if (!mpu.begin()) {
@@ -113,5 +133,27 @@ void loop() {
   Serial.println(" degC");
 
   Serial.println("");
+
+  // Connect to the MQTT to send data.
+  mqtt_client.check_connection(client_id);
+
+  // First, we need to convert the readings to JSON that matches the API of the
+  // backend.
+  DynamicJsonDocument doc(1024);
+  doc["temperature"] = temp.temperature;
+
+  // Create an array to store the rotation of x-, y-, and z-axis.
+  JsonArray angleValues = doc.createNestedArray("angle");
+  angleValues.add(g.gyro.x);
+  angleValues.add(g.gyro.y);
+  angleValues.add(g.gyro.z);
+
+  // Sending the JSON to MQTT.
+  String json_doc;
+  serializeJson(doc, json_doc);
+  char buf[500];
+  json_doc.toCharArray(buf, 500);
+  mqtt_client.publish_message(publish_topic, buf);
+
   delay(500);
 }
