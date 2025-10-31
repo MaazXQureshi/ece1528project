84e6616fedce22a4ec937e09a0301202dba980d7
Charles Hsieh <1hsiehcha@gmail.com>
2025-10-30 22:50:49 -0400

    [Project] Switched MPU6050 library to measure yaw, pitch, roll accurately.

--- sensor_folder/mpu6050/mpu6050.ino
+++ sensor_folder/mpu6050/mpu6050.ino
@@ -1,10 +1,12 @@
-#include "MPU6050.h"
+#include <MPU6050_light.h>
+#include "Wire.h"
 #include "MqttClient.h"
 #include "WifiClient.h"
 #include <ArduinoJson.h>
 
-// Initialize the MPU6050.
-MPU6050 mpu6050(MPU6050_RANGE_8_G, MPU6050_RANGE_500_DEG, MPU6050_BAND_5_HZ);
+// Initialize the MPU6050 light library.
+MPU6050 mpu(Wire);
+unsigned long timer = 0;
 
 // Wi-Fi login credientials (should modify according to the your Wi-Fi).
 char ssid_wifi[] = "BELL318";
@@ -26,54 +28,57 @@ void setup(void) {
     delay(10);
   }
 
-  // Print out the setup values of MPU6050.
-  mpu6050.printSetup();
-
   // Attempt to connect to the Wi-Fi and MQTT.
   wifi_client.connect();
   mqtt_client.connect(client_id);
+
+  // Setup for MPU6050 for the light library.
+  Wire.begin();
+  
+  byte status = mpu.begin();
+  Serial.print(F("MPU6050 status: "));
+  Serial.println(status);
+  while(status!=0){ } // stop everything if could not connect to MPU6050
+  
+  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  delay(1000);
+  // mpu.upsideDownMounting = true; // uncomment this line if the MPU6050 is mounted upside-down
+  mpu.calcOffsets(); // gyro and accelero
+  Serial.println("Done!\n");
 
   delay(100);
 }
 
 void loop() {
-  std::optional<sensors_event_t> a, g, temp;
-  std::tie(a, g, temp) = mpu6050.getSensorData();
+  mpu.update();
+  
+  // Print data every 500 ms
+  if((millis() - timer) > 500){
+    float angle_x = mpu.getAngleX();
+    float angle_y = mpu.getAngleY();
+    float angle_z = mpu.getAngleZ();
+    float temp = mpu.getTemp();
+    Serial.println("Degrees: X : " + String(angle_x) + ", Y: " + String(angle_y) + ", Z: " + String(angle_z));
+    Serial.println("Temp: " + String(temp));
 
-  if (a.has_value()) {
-    Serial.println("Acceleration X: " + String(a->acceleration.x) + ", Y: " + String(a->acceleration.y) + ", Z: " + String(a->acceleration.z) + " m/s^2");
-  }
+    timer = millis(); 
 
-  if (g.has_value()) {
-    Serial.println("Rotation X: " + String(g->gyro.x) + ", Y: " + String(g->gyro.y) + ", Z: " + String(g->gyro.z) + " rad/s");
-  }
-
-  if (temp.has_value()) {
-    Serial.println("Temperature: " + String(temp->temperature) + " degC");
-  }
-
-  if (a.has_value() && g.has_value() && temp.has_value()) {
     // Connect to the MQTT to send data.
     mqtt_client.check_connection(client_id);
 
     // First, we need to convert the readings to JSON that matches the API of the
     // backend.
     DynamicJsonDocument doc(1024);
-    doc["temperature"] = temp->temperature;
-
-    // Create an array to store the rotation of x-, y-, and z-axis.
-    JsonArray angleValues = doc.createNestedArray("angle");
-    angleValues.add(g->gyro.x);
-    angleValues.add(g->gyro.y);
-    angleValues.add(g->gyro.z);
+    doc["temperature"] = temp;
+    doc["roll"] = angle_x;
+    doc["pitch"] = angle_y;
+    doc["yaw"] = angle_z;
 
     // Sending the JSON to MQTT.
     String json_doc;
     serializeJson(doc, json_doc);
     char buf[500];
     json_doc.toCharArray(buf, 500);
-    mqtt_client.publish_message(publish_topic, buf);
+    mqtt_client.publish_message(publish_topic, buf); 
   }
-
-  delay(500);
 }
