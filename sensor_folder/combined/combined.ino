3e864ba4127f0f9f7f8d48c2226b993cac683b4c
Charles Hsieh <1hsiehcha@gmail.com>
2025-11-04 23:22:26 -0500

    [Project] Integrated the load cell, bottle ID, and new subscriber topic into the system.

--- sensor_folder/combined/combined.ino
+++ sensor_folder/combined/combined.ino
@@ -2,18 +2,27 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <LiquidCrystal_I2C.h>
+#include "HX711.h"
 #include "Wire.h"
 #include "MqttClient.h"
 #include "WifiClient.h"
 #include <ArduinoJson.h>
+#include <Arduino.h>
 
 #define SEND_INTERVAL 500
+#define DENSITY_OF_LIQUID_IN_GRAMS_PER_ML 1.0
 
 // Interval used to send data to MQTT.
 unsigned long timer = 0;
 
 // Initialize the MPU6050 light library.
 MPU6050 mpu(Wire);
+
+// Initialize the load cell and HX711.
+const int LOADCELL_DOUT_PIN = 12;
+const int LOADCELL_SCK_PIN = 13;
+#define CALIBRATION_FACTOR -416.4601307
+HX711 scale;
 
 // GPIO where the DS18B20 is connected to
 // Setup a oneWire instance to communicate with any OneWire devices
@@ -35,11 +44,22 @@ char pass_wifi[] = "A2761E9A66C7";
 WifiClient wifi_client(ssid_wifi, pass_wifi);
 
 // MQTT publisher settings. Be sure to change the IP address!
-const char* mqtt_broker_ip = "192.168.2.35";
+const char* mqtt_broker_ip = "192.168.2.43";
 const int mqtt_broker_port = 1883;
 const char* client_id = "publisher_sensors";
-const char* publish_topic = "sensors";
+const char* publish_topic = "MQTT_Sensor_Data";
 MqttClient mqtt_client(mqtt_broker_ip, mqtt_broker_port);
+
+// Helper function to convert 64-bit ID address into a string.
+String idToString(const DeviceAddress& deviceID) {
+  String final_result = "";
+
+  for (int i = 0; i < 8; ++i) {
+    final_result += String(deviceID[i], HEX);
+  }
+
+  return final_result;
+}
 
 void setup(void) {
   Serial.begin(115200);
@@ -67,6 +87,11 @@ void setup(void) {
   mpu.calcOffsets(); // gyro and accelero
   Serial.println("Done!\n");
 
+  // Intialize the load cell and HX711 sensor.
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(CALIBRATION_FACTOR); // Important: this value is obtained by calibrating the scale with known weights.
+  scale.tare(); // Reset the scale to 0.
+
   // Start the DS18B20 sensor.
   sensors.begin();
 
@@ -84,6 +109,11 @@ void loop() {
   mpu.update();
   
   if ((millis() - timer) > SEND_INTERVAL) {
+    // Load cell and HX711 sensor.
+    scale.power_up();
+    float weight_in_grams = scale.get_units();
+    float volume_in_milligrams = weight_in_grams / DENSITY_OF_LIQUID_IN_GRAMS_PER_ML;
+
     // MPU6050 sensor.
     float roll = mpu.getAngleX();
     float pitch = mpu.getAngleY();
@@ -96,9 +126,9 @@ void loop() {
 
     // Print the temperature to the LCD on the second row, first column.
     lcd.setCursor(0, 0);
-    lcd.print("Volume: ");
+    lcd.print(String(volume_in_milligrams) + " mL             ");
     lcd.setCursor(0, 1);
-    lcd.print("Temp: " + String(temperatureC) + " degC");
+    lcd.print("Temp: " + String(temperatureC) + " degC     ");
 
     // Light up the LED if the pitch is above a certain degree.
     if (abs(roll) > SPILLING_DEGREE) {
@@ -111,9 +141,18 @@ void loop() {
     // Connect to the MQTT to send data.
     mqtt_client.check_connection(client_id);
 
+    // Get the unique bottle ID to distinguish between different bottles.
+    // ID is obtained from DS18B20 which has a unique address per sensor.
+    DeviceAddress bottle_id;
+    sensors.getAddress(bottle_id, 0);
+    String bottle_id_string = idToString(bottle_id);
+    Serial.print("Bottle ID: " + bottle_id_string);
+
     // First, we need to convert the readings to JSON that matches the API of the
     // backend.
     DynamicJsonDocument doc(1024);
+    doc["bottle_id"] = bottle_id_string;
+    doc["volume"] = volume_in_milligrams;
     doc["temperature"] = temperatureC;
     doc["roll"] = roll;
     doc["pitch"] = pitch;
@@ -128,5 +167,8 @@ void loop() {
 
     // Update timer to accurately determine when to take the data sample again.
     timer = millis(); 
+
+    // Power down scale to save energy.
+    scale.power_down();
   }
 }
