19c9f97f04516181c91eab8ac20963c462ea4b3d
JP <joaoppgmarques@gmail.com>
2025-11-28 09:14:59 -0500

    MQTT update

--- bridge/mqtt_client.py
+++ bridge/mqtt_client.py
@@ -4,51 +4,76 @@ from mqtt_config import *
 def publish_actuator_commands():
     while keepRunning.is_set():
         for i in bottle_id: 
-            try:
-                resThresh = query(url=url + idx_threshold + "/" + str(i), method="GET")
-                resMode = query(url=url + idx_cleaning + "/" + str(i), method="GET")
-                if dic_bottles[i]["threshold"] != resThresh["threshold"] or dic_bottles[i]["cleaning"] != resMode["cleaning"]:
-                    dic_bottles[i]["threshold"] = resThresh["threshold"]
-                    dic_bottles[i]["cleaning"] = resMode["cleaning"]
-                    payload = {"TH":resThresh["threshold"],"CL":resMode["cleaning"]}
-                    mqttc.publish(topic=dic_topics_act[i], payload=json.dumps(payload), qos=1)
-            except:
-                print("Error forwarding actuator data to bottle " + str(i))
+            resThresh = query(url=url + idx_threshold + "/" + str(i), method="GET")
+            resClean = query(url=url + idx_cleaning + "/" + str(i), method="GET")
+            if dic_bottles[i]["th"] != resThresh["th"] or dic_bottles[i]["clean"] != resClean["clean"]:
+                dic_bottles[i]["th"] = resThresh["th"]
+                dic_bottles[i]["clean"] = resClean["clean"]
+                payload = {"th":resThresh["th"],"clean":resClean["clean"]}
+                mqttc.publish(topic=dic_topics_act[i], payload=json.dumps(payload), qos=1)
         sleep(interval_Act_Data)
+
 def message_processing():
     while keepRunning.is_set():
         if FIFO.empty() == False:
             msg = FIFO.get()
             payloadJson = json.loads(msg.payload)
-            print("MQTT - msg processing - " + msg.topic + " - " + str(payloadJson))
+            print("Msg processing - " + msg.topic + " - " + str(payloadJson))
             dict_id = int(payloadJson["id"])
-            if msg.topic == mqtt_registration:
-                bottle_register(dict_id)
-            elif flag_id[dict_id]:
-                del payloadJson["id"]
-                try:
-                    payloadJson={"volume":payloadJson["vol"],"roll":payloadJson["rol"],"pitch":payloadJson["pit"],"yaw":payloadJson["yaw"],"temperature":payloadJson["temp"]} #change this
-                    query(url=url + idx_reading + "/" + str(dict_id), method="POST", payload=payloadJson)
-                except:
-                    print("Error posting sensor data for bottle " + str(dict_id))
-            else:
+            if msg.topic == mqtt_reg_topic:
+                if bottle_reg_flag[dict_id] == False:
+                    bottle_register(dict_id)
+            elif bottle_reg_flag[dict_id] == False:
                 print("bottle " + str(dict_id) + " not registered")
-mqttc = connect(mqtt_broker_ip)
-mqttc.subscribe(topic=mqtt_sensor_topic, qos=0)
-mqttc.subscribe(topic=mqtt_registration, qos=1)
-try:
-    mqttc.loop_start()
-    keepRunning = threading.Event()
-    keepRunning.set()
-    message_Processor = threading.Thread(target=message_processing)
-    actuator_Controller = threading.Thread(target=publish_actuator_commands)
-    message_Processor.start()
-    actuator_Controller.start()
-    while(1):
-        pass
-except KeyboardInterrupt:
-    keepRunning.clear()
-    message_Processor.join()
-    actuator_Controller.join()
-    mqttc.disconnect()
+            elif msg.topic == mqtt_spill_topic:
+                active_flag[dict_id] = True
+                if payloadJson["seq"] > spill_seq[dict_id]:
+                    if payloadJson["spill"] == True:
+                        spill_flag[dict_id] = True
+                        print("Spill detected for bottle " + str(dict_id))
+                    else:
+                        spill_flag[dict_id] = False
+                        print("Spill cleared for bottle " + str(dict_id))
+                    #payloadJson = {"spill":spill_flag[dict_id]}
+                    del payloadJson["id"]
+                    del payloadJson["seq"]
+                    query(url=url + idx_spill + "/" + str(dict_id), method="POST", payload=payloadJson)
+                else:
+                    print("Out-of-sequence spill message for bottle " + str(dict_id))
+            elif msg.topic == mqtt_sensor_topic:
+                active_flag[dict_id] = True
+                if spill_flag[dict_id] == False:
+                    del payloadJson["id"]
+                    #payloadJson={"vol":payloadJson["vol"],"temp":payloadJson["temp"]} 
+                    query(url=url + idx_reading + "/" + str(dict_id), method="POST", payload=payloadJson)
+
+            else:
+                print("Unknown topic " + msg.topic)
+
+
+if __name__ == "__main__":
+    mqttc = connect(mqtt_broker_ip)
+    mqttc.subscribe(topic=mqtt_sensor_topic, qos=0)
+    mqttc.subscribe(topic=mqtt_reg_topic, qos=1)
+    mqttc.subscribe(topic=mqtt_spill_topic, qos=1)
+    try:
+        mqttc.loop_start()
+        message_Processor = threading.Thread(target=message_processing)
+        actuator_Controller = threading.Thread(target=publish_actuator_commands)
+        message_Processor.start()
+        actuator_Controller.start()
+        while 1:
+            sleep(60)
+            for i in bottle_id:
+                if active_flag[i] == False:
+                    bottle_deregister(i)
+                else:
+                    active_flag[i] = False
+                    
+
+    except KeyboardInterrupt:
+        keepRunning.clear()
+        message_Processor.join()
+        actuator_Controller.join()
+        mqttc.disconnect()
 
--- bridge/mqtt_config.py
+++ bridge/mqtt_config.py
@@ -3,46 +3,67 @@ import json, requests, queue, threading
 from time import sleep
 import numpy as np
 
-mqtt_broker_ip = "127.0.0.1"
+mqtt_broker_ip = "127.0.0.1" #change this to the broker IP
+
 mqtt_sensor_topic = "Sen_Data"
-mqtt_actuator_topic = "Act_Data"
-mqtt_registration = "Reg_Msg"
+mqtt_spill_topic = "Spill_Flag"
+mqtt_act_topic = "Act_Data"
+mqtt_reg_topic = "Reg_Msg"
+
 interval_Act_Data = 10 #in seconds
 
 url = "http://143.198.33.14:8080/"
 idx_reading = "readings"
 idx_threshold = "threshold"
 idx_cleaning = "cleaning"
+idx_spill = "spilling"
 
 dic_bottles = {}
 dic_topics_act = {}
 bottle_id = []
-flag_id = [False]*10
+bottle_reg_flag = [False]*10
+spill_flag = [False]*10
+active_flag = [False]*10
+spill_seq = [-1]*10
+
 FIFO = queue.Queue()
+keepRunning = threading.Event()
+keepRunning.set()
 
 def query(url, method, payload=None):
     try:
         response = requests.Response()
         if method == "GET":
             response = requests.get(url)
-        if method == "POST":
-            response = requests.post(url, data=json.dumps(payload), headers={"content-type": "application/json"})
-            print(method + " - " + str(response.status_code) + " - " + url)
-        if response.content is not None:
             return json.loads(response.content)
+        elif method == "POST":
+            requests.post(url, data=json.dumps(payload), headers={"content-type": "application/json"})
+            return
+        else:
+            print("Invalid method in query")
     except:
-        print(method + " - Connection error - " + url)
+        print(method + "-Connection error-" + url)
 
 def bottle_register(dict_id):
     bottle_id.append(dict_id) 
-    flag_id[dict_id] = True
-    dic_bottles[dict_id] = {"threshold": -1.0, "cleaning": 0}
-    dic_topics_act[dict_id] = mqtt_actuator_topic + "_" + str(dict_id)
+    bottle_reg_flag[dict_id] = True
+    dic_bottles[dict_id] = {"th": -1.0, "clean": 0}
+    dic_topics_act[dict_id] = mqtt_act_topic + "_" + str(dict_id)
+    active_flag[dict_id] = True
+    print("Bottle " + str(dict_id) + " registered.")
+    return
 
+def bottle_deregister(dict_id):
+    bottle_reg_flag[dict_id] = False
+    bottle_id.remove(dict_id)
+    del dic_bottles[dict_id]
+    del dic_topics_act[dict_id]
+    active_flag[dict_id] = False
+    print("Bottle " + str(dict_id) + " deregistered.")
+    return
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQT broker - rc:" + str(rc))
-
 
 def on_message(client, userdata, msg):
     FIFO.put(msg)
@@ -50,10 +71,8 @@ def on_message(client, userdata, msg):
 def on_subscribe(client, userdata, mid, granted_qos):
     print("MQTT - subscribed")
 
-
 def on_publish(client, userdata, mid):
     print("MQTT - published: " + str(mid))
-
 
 def connect(ip, port=1883):
     mqttc = mqtt.Client()
